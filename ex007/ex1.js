28b1376c81966fd3307c4b72760ea36a39cf05b1
Caio Cestari <145575652+CaioCL019@users.noreply.github.com>
2025-06-29 19:20:39 -0300

    aulas dom e condicoes

---
+++ ex007/ex1.js
@@ -0,0 +1,6 @@
+let vel = 100
+if (vel > 60){ // Condicao simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+console.log(`Dirija sempre usando cinto de segurança!`)
