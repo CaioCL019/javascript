a155149c0324aa17531c56efabc3508d4b221545
Caio Cestari <145575652+CaioCL019@users.noreply.github.com>
2026-08-17 18:37:26 -0300

    Create ex11
    Estruturas de repeticao

--- ex11/index.js
+++ ex11/index.js
@@ -1,1 +1,14 @@
-console.log('Tudo Bem?');
+
+
+/*let c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`);
+    c++
+}*/
+
+let c = 1
+
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while (c <= 10)
