96e52cf42a9e2621f696e5dfaf8f301a8caa1112
Caio Cestari <145575652+CaioCL019@users.noreply.github.com>
2026-08-17 17:23:33 -0300

    att ex10

--- ex10/index.js
+++ ex10/index.js
@@ -13,6 +13,40 @@ function verificar() {
         anoDigitado > anoAtual) {
         alert('[ERRO] TENTE NOVAMENTE!');
     } else {
-        alert('tudo ok');
+        const fsex = document.getElementsByName('radsex')
+        const idade = anoAtual - Number(fanoInput.value)
+        let genero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Crianca
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21){
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50){
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Crianca
+            } else if (idade < 21){
+                // Jovem
+            } else if (idade < 50){
+                // Adulto
+            } else {
+                //idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
